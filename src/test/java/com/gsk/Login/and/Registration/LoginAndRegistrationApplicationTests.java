dd6ec2faac05e55a5702d83bbe00857bc9524ed7
Gaurang Sharma <gaurangsharma.003@gmail.com>
2024-12-11 16:43:05 +0530

    first commit

---
+++ src/test/java/com/gsk/Login/and/Registration/LoginAndRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsk.Login.and.Registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
